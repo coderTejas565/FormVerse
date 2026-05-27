8b0a15f9c803cb7d8862ed8493a1186842ed81f9
Tejas Avhad <codingwithtejas7@gmail.com>
2026-05-27 19:33:55 +0530

    fix: update tsup configuration to include external handling for monorepo support

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -2,11 +2,23 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["./src/index.ts"],
+
+  format: ["cjs"],
+
   bundle: true,
-  external: [], // IMPORTANT
+
   splitting: false,
+
   clean: true,
+
   outDir: "./dist",
+
   minify: true,
+
   sourcemap: false,
+
+  external: [],
+
+  // IMPORTANT FIX FOR MONOREPO
+  noExternal: ["@repo/*"],
 });
